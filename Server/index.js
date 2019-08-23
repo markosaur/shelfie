98ad2594d945a538e3413223dc212674965947e2
Markosaur <mark3pace@gmail.com>
2019-08-23 15:13:11 -0600

    sql table is working, setting up form

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,27 @@
 //need to require packages and controller file
+require('dotenv').config()
 const express= require('express');
-const app= express();
+const massive= require('massive')
 const ctrl= require('./controller'); 
 
+const app= express();
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+//middleware
+
+massive(CONNECTION_STRING)
+    .then(dbInstance => {
+        app.set('db', dbInstance);
+    })
+    .catch(err => console.log(err));
+
+
+// this is where the endpoints will go, we will add these later
 app.use(express.json())
 
-//this is where the endpoints will go, we will add these later
 
-
-const port= 4000;
-app.listen(port, ()=> console.log(`Here comes the sun ${port}`))
+// const port= 4000;
+app.listen(SERVER_PORT, ()=> {
+    console.log(`Here comes the sun ${SERVER_PORT}`)
+})
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,9 +1,26 @@
 import React, {Component} from 'react'
 
 class Form extends Component {
+    constructor(){
+        super()
+        this.state={
+            name: '',
+            price:0,
+            imgurl: ''
+        }
+    }
+
     render(){
         return (
-        <h1>Form</h1>
+        <div>
+            <input placeholder= "name"></input>
+            <input placeholder= "price"></input>
+            <input placeholder= "image_url"></input>
+            <button>Add Inventory</button>
+            <button>Cancel Inventory</button>
+
+        {/* <h1>Form</h1> */}
+        </div>
         )
     }   
 } 
